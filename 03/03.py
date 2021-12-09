e594698bf498eca6c30da11d4e67cb56c7693802
oswinoswin <laura.uchwat@gmail.com>
2021-12-09 15:04:15 +0100

    03

--- 03/03.py
+++ 03/03.py
@@ -1,7 +1,33 @@
 def first(filename):
     with open(filename, "r") as f:
+        line_counter = 0
+        gamma = None
         for line in f:
-            print(line)
+            if gamma == None:
+                gamma = [0 for _ in range(len(line) - 1)]
+            for i in range(len(gamma)):
+                gamma[i] += int(line[i])
+
+            line_counter += 1
+        print(gamma)
+
+        epsilon = ["0" for _ in gamma]
+        for i in range(len(gamma)):
+            if gamma[i] > line_counter - gamma[i]:
+                gamma[i] = "1"
+                epsilon[i] = "0"
+            else:
+                gamma[i] = "0"
+                epsilon[i] = "1"
+        gamma_value_str = "".join([x for x in gamma])
+        epsilon_value_str = "".join([x for x in epsilon])
+        print(gamma_value_str)
+        gamma_value = int(gamma_value_str, 2)
+        epsilon_value = int(epsilon_value_str, 2)
+        print(epsilon_value_str)
+        return gamma_value * epsilon_value
+
+
 
 
 def second(filename):
@@ -13,5 +39,5 @@ def second(filename):
 if __name__ == "__main__":
     print(first("example.txt"))
     print(first("input.txt"))
-    print(second("example.txt"))
-    print(second("input.txt"))
+    # print(second("example.txt"))
+    # print(second("input.txt"))
