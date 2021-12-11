74fdd4bf7b2d0bf3ea897453bd2fdfeb86874a49
oswinoswin <laura.uchwat@gmail.com>
2021-12-11 15:35:35 +0100

    09

--- 09/09.py
+++ 09/09.py
@@ -1,7 +1,38 @@
+import re
+import numpy as np
+
 def first(filename):
+    p = re.compile(r'\d')
+    heights = []
     with open(filename, "r") as f:
         for line in f:
-            print(line)
+            row = [int(x) for x in p.findall(line)]
+            heights.append(row)
+    heights = np.array(heights)
+    risk_sum = 0
+    rows, columns = heights.shape
+    for row_idx in range(rows):
+        for col_idx in range(columns):
+            neighbours = []
+            current = heights[row_idx, col_idx]
+            if row_idx >= 1:
+                top = heights[row_idx - 1, col_idx]
+                neighbours.append(top)
+            if row_idx < rows - 1:
+                bottom = heights[row_idx + 1, col_idx]
+                neighbours.append(bottom)
+            if col_idx >= 1:
+                left = heights[row_idx, col_idx - 1]
+                neighbours.append(left)
+            if col_idx < columns - 1:
+                right = heights[row_idx, col_idx + 1]
+                neighbours.append(right)
+            if current < min(neighbours):
+                risk_sum += current + 1
+    return risk_sum
+
+
+
 
 
 def second(filename):
@@ -13,5 +44,5 @@ def second(filename):
 if __name__ == "__main__":
     print(first("example.txt"))
     print(first("input.txt"))
-    print(second("example.txt"))
-    print(second("input.txt"))
+    # print(second("example.txt"))
+    # print(second("input.txt"))
